eee9196dd63b648e6f5a36cec099b89d31eba6ca
Admin <vishlin.a.k@gmail.com>
2025-04-28 21:19:40 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("Человек совершеннолетний");
+        }
+        if (age < 18) {
+            System.out.println("Человек не достиг совершеннолетия");
+        }
     }
-}
+
+    }
+
+
+
+
