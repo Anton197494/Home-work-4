68dc6ceb26a6863dfec09075cbee8c9771493c8c
Admin <vishlin.a.k@gmail.com>
2025-05-01 21:15:43 +0300

    Task 7

--- src/Main.java
+++ src/Main.java
@@ -72,6 +72,20 @@ public class Main {
             System.out.println("Стоячих мест в вагоне не осталось");
         }
 
+        int one = 1;
+        int two = 2;
+        int three = 3;
+        if (one > two &&  one > three) {
+            System.out.println("Первое число самое большое");
+        }
+        if (two > one &&  two > three) {
+            System.out.println("Второе число самое большое");
+        }
+        else {
+            System.out.println("Третье число самое большое");
+        }
+
+
 
 
 
